a9330ef4e8180833d026dd98a6a3251e845b789a
Regina <199090935+rehinacadiz@users.noreply.github.com>
2026-04-20 22:40:59 +0800

    Updated App.py

--- backend/user/apps.py
+++ backend/user/apps.py
@@ -2,4 +2,9 @@ from django.apps import AppConfig
 
 
 class UserConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
     name = 'user'
+    verbose_name = 'User Management'
+
+    def ready(self):
+        pass  # Reserved for signal imports if needed later
